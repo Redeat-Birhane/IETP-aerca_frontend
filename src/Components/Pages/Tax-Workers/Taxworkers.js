4290696196d6cbd23e0a8c7387eddfcc8393b84a
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-12-23 01:27:47 +0300

    fix taxworker.js

--- src/Components/Pages/Tax-Workers/Taxworkers.js
+++ src/Components/Pages/Tax-Workers/Taxworkers.js
@@ -2,20 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Taxworkers.css";
 const API_BASE = process.env.REACT_APP_API_BASE;
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.startsWith(name + "=")) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-}
+
 
 export default function TaxWorkers() {
   const navigate = useNavigate();
