5d4710ba150b74463091a7adba79608d13f3ee85
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-12-23 01:33:15 +0300

    fix taxworker.js

--- src/Components/Pages/Tax-Workers/Taxworkers.js
+++ src/Components/Pages/Tax-Workers/Taxworkers.js
@@ -221,20 +221,7 @@ export default function TaxWorkers() {
     </div>
   );
 }
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.startsWith(name + "=")) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-}
+
 
 export default function TaxWorkers() {
   const navigate = useNavigate();
